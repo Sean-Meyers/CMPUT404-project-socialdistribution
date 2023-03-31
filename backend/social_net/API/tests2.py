77ee8a00d8a25785be927b4797d966712beeddcf
breadtitor <qg@ualberta.ca>
2023-03-30 21:50:15 -0600

    Update tests2.py

--- backend/social_net/API/tests2.py
+++ backend/social_net/API/tests2.py
@@ -19,7 +19,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import User
+
 
 
 # from .models import Post, Comment, Like , Author
